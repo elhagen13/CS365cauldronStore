b2e519bb49388ec91e00e9f3b2a4000ce14bc18b
Ella Hagen <elhagen@calpoly.edu>
2023-09-28 20:47:49 -0700

    redo catalog

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -1,27 +1,22 @@
 from fastapi import APIRouter
-import sqlalchemy
-from src import database as db
 
 router = APIRouter()
 
+
 @router.get("/catalog/", tags=["catalog"])
 def get_catalog():
-    with db.engine.begin() as connection:
-        sql_to_execute = """ 
-            SELECT num_red_potions FROM global_inventory
-            """
+    """
+    Each unique item combination must have only a single price.
+    """
 
-        result = connection.execute(sqlalchemy.txt(sql_to_execute))
-        first_row = result.first()
+    # Can return a max of 20 items.
 
-        # Can return a max of 20 items.
-
-        return [
-                {
-                    "sku": "RED_POTION_0",
-                    "name": "red potion",
-                    "quantity": first_row.num_red_potions,
-                    "price": 50,
-                    "potion_type": [100, 0, 0, 0],
-                }
-            ]
+    return [
+            {
+                "sku": "RED_POTION_0",
+                "name": "red potion",
+                "quantity": 1,
+                "price": 50,
+                "potion_type": [100, 0, 0, 0],
+            }
+        ]
---
+++ src/api/trial.py
@@ -0,0 +1,25 @@
+from fastapi import APIRouter
+import sqlalchemy
+from src import database as db
+
+router = APIRouter()
+
+@router.get("/catalog/", tags=["catalog"])
+def get_catalog():
+    with db.engine.begin() as connection:
+        sql_to_execute = """ 
+            SELECT num_red_potions FROM global_inventory
+            """
+
+        result = connection.execute(sqlalchemy.txt(sql_to_execute))
+        first_row = result.first()
+        # Can return a max of 20 items.
+        return [
+                {
+                    "sku": "RED_POTION_0",
+                    "name": "red potion",
+                    "quantity": first_row.num_red_potions,
+                    "price": 50,
+                    "potion_type": [100, 0, 0, 0],
+                }
+            ]
